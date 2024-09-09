2ba98700c5b1ad808dbcfb2856baa8494f7c787b
luckycmc <2287232750@qq.com>
2024-09-09 14:33:52 +0800

    update tcp server

--- tcp/server/main.go
+++ tcp/server/main.go
@@ -1,3 +1,11 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
 func process(conn net.Conn) {
 	defer conn.Close()
 	for {
@@ -11,5 +19,21 @@ func process(conn net.Conn) {
 		recvStr := string(buf[:n])
 		fmt.Println("收到client端发来的数据: ", recvStr)
 		conn.Write([]byte(recvStr))
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("listen failed, err:", err)
+		return
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		}
+		go process(conn)
 	}
 }
