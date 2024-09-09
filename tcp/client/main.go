3708b1cf9cfe78eb5986d428011b307eedd83d72
luckycmc <2287232750@qq.com>
2024-09-09 14:45:19 +0800

    create tcp client

---
+++ tcp/client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := inputReader.ReadString('\n')
+		inputInfo := strings.Trim(input, "\r\n")
+		if strings.ToUpper(inputInfo) == "Q" {
+			return
+		}
+
+		_, err := conn.Write([]byte(inputInfo + "\n"))
+		if err != nil {
+			fmt.Println("Error writing:", err)
+			return
+
+		}
+		buf := [512]byte{}
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			return
+		}
+		fmt.Println(string(buf[:n]))
+	}
+}
