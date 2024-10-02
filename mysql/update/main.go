3bf0b2ac08189406c48d378a1ffc2944fa8faa52
luckycmc <2287232750@qq.com>
2024-10-02 10:43:17 +0800

    mysql-select

---
+++ mysql/update/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+import _ "github.com/go-sql-driver/mysql"
+
+var Db *sqlx.DB
+
+func init() {
+	database, err := sqlx.Open("mysql", "root:root@tcp(192.168.72.130:3306)/test")
+	if err != nil {
+		fmt.Println("open mysql failed", err)
+		return
+	}
+	Db = database
+}
+
+func main() {
+	res, err := Db.Exec("update user set email=? where id=?", "tes111t@email.com", 1)
+	if err != nil {
+		fmt.Println("update failed", err)
+		return
+	}
+	row, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("rows affected failed", err)
+	}
+	fmt.Println("update", row)
+}
